cf7ccaf50399e0e85117fa9a17e41d3eb0a467d3
Hector Davensburg <hector.davensburg@gmail.com>
2021-12-21 12:03:50 +0100

    Add handler to textbox

--- static/script.js
+++ static/script.js
@@ -1,37 +1,4 @@
 
-$(document).on('keypress',function(e) {
-  if(e.which == 13) {
-    var current_input = $(".current input");
-    var output = current_input.val();
-
-    current_input.attr("readonly", "");
-
-    $.ajax({
-      url: "/word",
-      method: "POST",
-      contentType: 'application/json',
-      processData: false,
-      data: JSON.stringify({"word": output})
-    }).done(function() {
-      if(current_input.val() === "die Haut") {
-        var new_node = $("#right-word").clone();
-      } else {
-        var new_node = $("#wrong-word").clone();
-      }
-
-      new_node.attr("style", "");
-
-      $(".current").before(new_node);
-
-      current_input.val("");
-
-      // update the size of the words
-      var size = $(".words").css("height");
-      $(".centering-box").css("top", "calc(50% - " + size + ")");
-    });
-
-  }
-});
 
 $(document).ready(function() {
 
@@ -41,6 +8,43 @@ $(document).ready(function() {
   }).done(function(data) {
     var word = data.word;
     $("#current-input").text(word.input);
+  });
+
+  $("#current-output").keyup(function(e) {
+
+    if($(this).attr('readonly'))
+      return;
+
+    if(e.which == 13) {
+      var current_input = $(".current input");
+      var output = current_input.val();
+
+      current_input.attr("readonly", "");
+
+      $.ajax({
+        url: "/word",
+        method: "POST",
+        contentType: 'application/json',
+        processData: false,
+        data: JSON.stringify({"word": output})
+      }).done(function() {
+        if(current_input.val() === "die Haut") {
+          var new_node = $("#right-word").clone();
+        } else {
+          var new_node = $("#wrong-word").clone();
+        }
+
+        new_node.attr("style", "");
+
+        $(".current").before(new_node);
+
+        current_input.val("");
+
+        // update the size of the words
+        var size = $(".words").css("height");
+        $(".centering-box").css("top", "calc(50% - " + size + ")");
+      });
+    }
   });
 
 });
