99879c1fbd32822ca5fcf3d4a1cd70d7a73ee6aa
Hector Davensburg <hector.davensburg@gmail.com>
2021-12-21 16:55:09 +0100

    Load test vocabulary
    Display vocabulary for each option

--- server.py
+++ server.py
@@ -9,33 +9,56 @@ from learn import Vocabulary, LearnEngine, Word
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+engine = None
+
+
+class WordInput(BaseModel):
+    word: str
 
 
 class WordOutput(BaseModel):
     word: str
 
 
-def json_word(word: Word):
-    return {
-        "input": word.word,
-        "result": word.word_french
-    }
+class WordResult(BaseModel):
+    success: bool
+
+    word_input: WordInput
+    word_output: WordOutput
+
+    hint: Optional[str]
+
+    # if None => no more word
+    next_word: Optional[WordInput]
+
 
 @app.get("/")
 def root():
     return RedirectResponse(url='/static/learn.html')
 
 @app.post("/new_session")
-def new_session():
+def new_session() -> WordInput:
+    global engine
     new_voc = Vocabulary.load("vocabulary/test.txt")
-
     engine = LearnEngine(new_voc)
+
     current_word = engine.current_word
 
-    return {
-        "word": json_word(current_word)
-    }
+    return WordInput(word=current_word.word_french)
 
 @app.post("/word")
 def post_word(word: WordOutput):
-    return {}
+    current_word = engine.current_word
+    hint_word = current_word.word
+
+    success = engine.guess(word.word)
+
+    next_word = None
+    if engine.current_word is not None:
+        next_word = WordInput(word=engine.current_word.word_french)
+
+    return WordResult(success=success,
+                      hint=hint_word,
+                      word_input=WordInput(word=current_word.word_french),
+                      word_output=word,
+                      next_word=next_word)
--- static/script.js
+++ static/script.js
@@ -6,8 +6,7 @@ $(document).ready(function() {
     url: "/new_session",
     type: "POST"
   }).done(function(data) {
-    var word = data.word;
-    $("#current-input").text(word.input);
+    $("#current-input").text(data.word);
   });
 
   $("#current-output").keyup(function(e) {
@@ -16,10 +15,11 @@ $(document).ready(function() {
       return;
 
     if(e.which == 13) {
-      var current_input = $(".current input");
-      var output = current_input.val();
+      var current_input = $("#current-input");
+      var current_output = $("#current-output");
+      var output = current_output.val();
 
-      current_input.attr("readonly", "");
+      current_output.attr("readonly", true);
 
       $.ajax({
         url: "/word",
@@ -27,18 +27,28 @@ $(document).ready(function() {
         contentType: 'application/json',
         processData: false,
         data: JSON.stringify({"word": output})
-      }).done(function() {
-        if(current_input.val() === "die Haut") {
+      }).done(function(result) {
+
+        if(result.success) {
           var new_node = $("#right-word").clone();
         } else {
           var new_node = $("#wrong-word").clone();
         }
 
         new_node.attr("style", "");
+        new_node.find(".input .field").text(result.word_input.word);
+        new_node.find(".output .field").text(result.word_output.word);
+        new_node.find(".result .field").text(result.hint);
 
         $(".current").before(new_node);
 
-        current_input.val("");
+        if(result.next_word) {
+          current_input.text(result.next_word.word)
+          current_output.val("");
+          current_output.attr("readonly", false);
+        } else {
+          $("#current-word").hide();
+        }
 
         // update the size of the words
         var size = $(".words").css("height");
